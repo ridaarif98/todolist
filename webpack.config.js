f708a7154be029d5c56f3ed8109b7c6bf6b5910c
ridaarif98 <ridaarif16@gmail.com>
2021-08-17 10:29:09 +0200

    Intsall webpack server and add markup for index.html

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,9 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  devServer: {
+      contentBase: './dist',
+    },
   plugins: [new HtmlWebpackPlugin({
     template: './src/index.html',
   })],
